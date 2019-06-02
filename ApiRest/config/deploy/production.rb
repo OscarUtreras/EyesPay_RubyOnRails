24f6847105a2cbcad51cd952733eb089420eeade
Oscar <oscar.utreras@usach.cl>
2019-06-02 17:45:58 -0400

    Fix deploy config

--- ApiRest/config/deploy/production.rb
+++ ApiRest/config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '3.14.146.252', user: 'deploy', roles: %w{app db web}
+server '3.14.146.252', user: 'ubuntu', roles: %w{app db web}
 
 # role-based syntax
 # ==================
